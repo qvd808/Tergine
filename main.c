5ee7335f3c6bcb2ed51dc697450ac401115c0423
Quang Vinh Dang <dqvinh101@gmail.com>
2024-06-21 21:07:08 -0700

    Update file

--- main.c
+++ main.c
@@ -10,6 +10,22 @@
 int running = 1;
 void exit_program_handler(int n);
 
+struct Point3d {
+	float x;
+	float y;
+	float z;
+};
+
+struct Cube {
+	struct Point3d p1;
+	struct Point3d p2;
+	struct Point3d p3;
+	struct Point3d p4;
+	struct Point3d p5;
+	struct Point3d p6;
+};
+
+
 int main() {
 
 	signal(SIGINT, exit_program_handler);
@@ -22,16 +38,16 @@ int main() {
 		// draw_right_triangle(0, 0, size, size);
 		// draw_rect(0, 0, size, size);
 		struct Point p1 = {
+			.x = 16,
+			.y = 40
+		};
+		struct Point p2 = {
 			.x = 0,
 			.y = 0
 		};
-		struct Point p2 = {
-			.x = 3,
-			.y = 8 
-		};
 		struct Point p3 = {
 			.x = 0,
-			.y = 8 
+			.y = 8
 		};
 		draw_triangle(p1, p2, p3);
 		// draw_line(p1,p3);
